aa6959dcca686832bf0e5c62abeb010e0da031d6
Andrew Michienzi <andrew@grcmc.org>
2024-10-05 11:38:59 -0400

    hurt box and health

--- scenes/player/gun/bullet.gd
+++ scenes/player/gun/bullet.gd
@@ -2,6 +2,8 @@ extends Area2D
 class_name Bullet
 
 @export var speed: float = 3000
+@export var damage_per_bullet = 1
+
 @onready var direction: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
