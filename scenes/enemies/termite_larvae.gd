a518a445d97335648c2fa71938ee6646146a978d
Andrew Michienzi <andrew@grcmc.org>
2024-10-05 15:11:18 -0400

    some andrew scene stuff

--- scenes/enemies/termite_larvae.gd
+++ scenes/enemies/termite_larvae.gd
@@ -5,10 +5,15 @@ class_name TermiteLarvae
 @export var velocity_component: VelocityComponent
 @export var start_direction: Vector2 = Vector2.RIGHT
 
+@export var toggle_direction_timer: Timer
+
 @onready var current_direction: Vector2
 
 func _ready() -> void:
 	current_direction = start_direction
+	toggle_direction_timer.autostart = false
+	toggle_direction_timer.start()
+	toggle_direction_timer.timeout.connect(_on_toggle_direction_timer_timeout)
 	pass # Replace with function body.
 
 
@@ -16,8 +21,12 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor() and not is_on_wall() and not is_on_ceiling():
 		velocity_component.apply_gravity(delta)
+		
+	if is_on_wall() and is_on_floor():
+		toggle_current_direction()
 	velocity_component.apply_run(current_direction)
 	velocity_component.do_move(self)
+	
 	pass
 
 func _on_died() -> void:
@@ -32,4 +41,5 @@ func toggle_current_direction() -> void:
 
 func _on_toggle_direction_timer_timeout() -> void:
 	toggle_current_direction()
+	toggle_direction_timer.start()
 	pass # Replace with function body.
