9df0c7e8dbacb4a3fdefff590ca29e4e6a9a2310
deruitda <deruitda@mail.gvsu.edu>
2024-10-05 22:17:12 -0400

    Better spawning

--- scenes/core/enemy_spawner.gd
+++ scenes/core/enemy_spawner.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.spawn_enemy.connect(spawn_enemy)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func spawn_enemy(scene: PackedScene, pos: Vector2) -> void:
--- scenes/enemies/spawner.gd
+++ scenes/enemies/spawner.gd
@@ -2,10 +2,19 @@ extends Node2D
 
 @export var scene : PackedScene
 @export var timer : Timer
+@export var animSprite : AnimatedSprite2D
 
 func _ready() -> void:
 	timer.timeout.connect(_on_spawn_timer_timeout)
+	animSprite.animation_finished.connect(_on_anim_finished)
 
 func _on_spawn_timer_timeout() -> void:
-	print("times up")
-	SignalBus.spawn_enemy.emit(scene, global_position)
+	# play the 'spawn' animation, when that animation is complete it will trigger 
+	# the _on_anim_finished callback to spawn the enemy scene
+	animSprite.play("spawn")
+
+func _on_anim_finished() -> void:
+	print(animSprite.animation)
+	if animSprite.animation == "spawn":
+		SignalBus.spawn_enemy.emit(scene, global_position)
+		animSprite.play("idle")
