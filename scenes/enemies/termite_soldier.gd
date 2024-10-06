fc9497a20087d439f9e007012f8ca2e2b6a3dbf4
Andrew Michienzi <andrew@grcmc.org>
2024-10-06 11:19:35 -0400

    soldier dying

--- scenes/enemies/termite_soldier.gd
+++ scenes/enemies/termite_soldier.gd
@@ -14,6 +14,8 @@ class_name TermiteSoldier
 @onready var _rage_speed: float = 600.0
 @onready var _walk_speed: float = 300.0
 
+@onready var is_dying: bool = false
+
 func _ready() -> void:
 	if start_walking_up:
 		enemy_walk_direction.set_current_direction(Vector2.UP)
@@ -35,31 +37,19 @@ func _process(delta: float) -> void:
 		animated_sprite_2d.flip_h = true
 		rage_ray.rotation = deg_to_rad(90)
 	
-	if animated_sprite_2d.animation == "walking" and is_raging:
+	if is_dying and animated_sprite_2d.animation != "dying":
+		animated_sprite_2d.play("dying")
+	elif animated_sprite_2d.animation == "walking" and is_raging:
 		animated_sprite_2d.play("raging")
 	elif animated_sprite_2d.animation == "raging" and not is_raging:
 		animated_sprite_2d.play("walking")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if player_is_in_rage_view():
-		is_raging = true
-		velocity_component.max_speed = _rage_speed
-		
+	if is_dying:
+		velocity_component.apply_gravity(delta)
 	else:
-		is_raging = false
-		velocity_component.max_speed = _walk_speed
-	
-	if is_on_wall() and (is_on_ceiling() || is_on_floor()):
-		enemy_walk_direction.toggle_current_direction()
-	
-	if is_on_wall():
-		velocity_component.apply_move(enemy_walk_direction.current_direction, delta)
-	elif not is_on_wall():
-		var wall_direction = Vector2.RIGHT
-		if not wall_is_right:
-			wall_direction = Vector2.LEFT
-		velocity_component.apply_move(wall_direction, delta)
+		handle_apply_movement(delta)
 	velocity_component.do_character_move(self)
 	
 	pass
@@ -78,4 +68,36 @@ func player_is_in_rage_view() -> bool:
 
 func _on_toggle_direction_timer_timeout() -> void:
 	enemy_walk_direction.toggle_current_direction()
+	pass # Replace with function body.
+
+func handle_apply_movement(delta: float) -> void:
+	
+	if player_is_in_rage_view():
+		is_raging = true
+		velocity_component.max_speed = _rage_speed
+	else:
+		is_raging = false
+		velocity_component.max_speed = _walk_speed
+	
+	if is_on_wall() and (is_on_ceiling() || is_on_floor()):
+		enemy_walk_direction.toggle_current_direction()
+	
+	if is_on_wall():
+		velocity_component.apply_move(enemy_walk_direction.current_direction, delta)
+	elif not is_on_wall():
+		var wall_direction = Vector2.RIGHT
+		if not wall_is_right:
+			wall_direction = Vector2.LEFT
+		velocity_component.apply_move(wall_direction, delta)
+
+func _on_health_component_died() -> void:
+	is_dying = true
+	pass # Replace with function body.
+
+func handle_death() -> void:
+	queue_free()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite_2d.animation == "dying":
+		handle_death()
 	pass # Replace with function body.
