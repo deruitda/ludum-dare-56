a37d1456cc84d47c0d5fea96d991d1c4bf888de5
Andrew Michienzi <andrew@grcmc.org>
2024-10-07 00:19:52 -0400

    CUT SCENE TIMING

--- scenes/levels/opening_scene.gd
+++ scenes/levels/opening_scene.gd
@@ -1,11 +1,49 @@
 extends Node2D
 @onready var rex: CutSceneRex = $Rex
+@onready var wait_timer: Timer
+
+
+var INITIAL_IDLE_TIME = 0.1
+var IDLE_BEFORE_PUTTING_IN_HEADPHONES = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	rex.start_walk(Vector2.RIGHT)
+	wait_timer = Timer.new()
+	add_child(wait_timer)
+	wait_timer.wait_time = 1.0
+	wait_timer.autostart = false
+	wait_timer.one_shot = true
+	wait_timer.start()
+	wait_timer.timeout.connect(_on_end_walk_in_timer_timeout)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	rex.apply_walk(Vector2.RIGHT, delta)
+	pass # Replace with function body.
+
+func _on_end_walk_in_timer_timeout() -> void:
+	rex.stop_walk()
+	wait_timer.timeout.disconnect(_on_end_walk_in_timer_timeout)
+	
+	wait_timer.wait_time = INITIAL_IDLE_TIME
+	wait_timer.start()
+	wait_timer.timeout.connect(_on_wait_after_entering_house_timer_timeout)
+
+func _on_wait_after_entering_house_timer_timeout() -> void:
+	
+	rex.start_walk(Vector2.RIGHT)
+	wait_timer.timeout.disconnect(_on_wait_after_entering_house_timer_timeout)
+	
+	wait_timer.wait_time = 3.16
+	wait_timer.start()
+	wait_timer.timeout.connect(_on_end_walk_to_hole_timer_timeout)
+	
+func _on_end_walk_to_hole_timer_timeout() -> void:
+	rex.stop_walk()
+	wait_timer.timeout.disconnect(_on_end_walk_to_hole_timer_timeout)
+	
+	wait_timer.wait_time = IDLE_BEFORE_PUTTING_IN_HEADPHONES
+	
+	pass
--- scenes/player/sprites/cut_scene_rex.gd
+++ scenes/player/sprites/cut_scene_rex.gd
@@ -1,6 +1,10 @@
 extends Node2D
 class_name CutSceneRex
 @onready var walk_speed: float = 400.0
+@onready var is_walking: bool = false
+@onready var walking_direction: Vector2
+
+@onready var lower_body: AnimatedSprite2D = $LowerBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +13,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if is_walking:
+		position += Vector2.RIGHT * walk_speed * delta
+	
 	pass
 
-func apply_walk(direction: Vector2, delta: float):
-	position += Vector2.RIGHT * walk_speed * delta
+func start_walk(new_direction: Vector2):
+	is_walking = true
+	walking_direction = new_direction
+	lower_body.play("walking")
+	
+	
+func stop_walk() -> void:
+	is_walking = false
+	lower_body.play("idle")
