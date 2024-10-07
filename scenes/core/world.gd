ef6f335c452a7d50c286fa3e934e795b5d3a054c
Andrew Michienzi <andrew@grcmc.org>
2024-10-07 00:58:47 -0400

    opening cut scene

--- scenes/core/world.gd
+++ scenes/core/world.gd
@@ -6,14 +6,19 @@ const OPENING_SCENE = preload("res://scenes/levels/opening_scene.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.start_game.connect(start_opening_cutscene) # Replace with function body.
-	SignalBus.opening_cutscene_finished.connect(start_opening_cutscene)
+	SignalBus.opening_cutscene_finished.connect(on_load_level)
 
 func start_opening_cutscene():
+	clean_up_scene()
 	add_child(OPENING_SCENE.instantiate())
 
 func on_load_level() -> void:
+	clean_up_scene()
 	add_child(current_level.instantiate())
 	
+func clean_up_scene():
+	for child in get_children():
+		child.queue_free()  # Properly removes and frees memory
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
