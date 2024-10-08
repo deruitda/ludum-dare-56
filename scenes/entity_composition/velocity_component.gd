9a9ee6be44d45e64ce13b8a81a154299b1beffe2
amichie1 <34970861+amichie1@users.noreply.github.com>
2024-10-08 10:10:32 -0400

    tweaking movement

--- scenes/entity_composition/velocity_component.gd
+++ scenes/entity_composition/velocity_component.gd
@@ -5,11 +5,16 @@ class_name VelocityComponent
 @export var max_speed: float = 1000.0
 @export var acceleration = 3000.0
 @export var in_air_acceleration: float = 3000.0
+@export var in_air_deceleration: float = 30000.0
 @export var in_air_resistance: float = 300.0
 @export var jump_velocity: float = 1200.0
 @export var wall_jump_velocity: float = 1500.0
 @export var wall_slide_speed: float = 50.0
 @export var floor_resistance: float = 8000.0
+
+@export var wall_jump_time: float = 0.5
+@onready var time_after_wall_jump: float = 0.0
+@onready var is_wall_jumping: bool = false
 
 @export var deceleration: float = 30000.0
 
@@ -24,6 +29,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _physics_process(delta: float) -> void:
+	
+	if is_wall_jumping:
+		time_after_wall_jump += delta
+		if time_after_wall_jump > wall_jump_time:
+			is_wall_jumping = false
+			time_after_wall_jump = 0.0
+	pass
 
 func apply_wall_jump(direction: Vector2) -> void:
 	if direction == Vector2.RIGHT:
@@ -33,6 +47,8 @@ func apply_wall_jump(direction: Vector2) -> void:
 	else:
 		assert("Direction should be either left or right when wall jumping")
 	velocity.y = -jump_velocity
+	is_wall_jumping = true
+	
 	
 	# Determine wall side and apply horizontal force
 func apply_floor_jump() -> void:
@@ -62,7 +78,16 @@ func apply_move(direction: Vector2, delta: float) -> void:
 		# Otherwise, apply regular acceleration
 		velocity.y = clamp(velocity.y + (direction.y * acceleration_metric * delta), -max_speed, max_speed)
 func apply_in_air_movement(direction: float, delta: float) -> void:
-	velocity.x = clamp(velocity.x + direction * in_air_acceleration * delta, -max_speed, max_speed)
+	var acceleration_metric = in_air_acceleration
+	var deceleration_metric = in_air_deceleration # Add a separate deceleration for air if needed
+
+	# Handle X-axis movement in air
+	if not is_wall_jumping and sign(direction) != sign(velocity.x) and velocity.x != 0:
+		# If the direction is opposite to the current velocity, apply deceleration
+		velocity.x = clamp(velocity.x + (direction * deceleration_metric * delta), -max_speed, max_speed)
+	else:
+		# Otherwise, apply regular acceleration
+		velocity.x = clamp(velocity.x + (direction * acceleration_metric * delta), -max_speed, max_speed)
 
 func apply_in_air_idle(delta: float):
 	velocity.x = move_toward(velocity.x, 0, in_air_resistance * delta)
